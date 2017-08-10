c127749840f1c0062d0cb77c582b85b4b0d4359f
Adrian Solis <aosolis@microsoft.com>
2017-08-09 23:35:32 -0700

    Populate repo from existing sample

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # teams-tab-identity
-Sample showing getting the id token of the user
+Sample showing getting the id token of the user from a tab in Microsoft Teams.
---
+++ auth-config.js
@@ -0,0 +1,9 @@
+// ADAL.js configuration
+let config = {
+    // The id of the AAD app
+    clientId: "f8aa5a15-a868-47b9-8586-ba5467286ce6",
+    // AAD app config should have this as a valid redirect uri
+    redirectUri: "https://aosolis.github.io/tab-idtoken/auth-done.html",
+    cacheLocation: "localStorage",
+    navigateToLoginRequestUrl: false,
+};
